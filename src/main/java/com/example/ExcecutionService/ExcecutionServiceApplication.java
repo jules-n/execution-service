1bba3a45d65663e0a442091a1b8eed9937071aab
ynero <ynero@softserveinc.com>
2021-08-04 19:48:31 +0300

    Generated structure

---
+++ src/main/java/com/example/ExcecutionService/ExcecutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExcecutionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcecutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcecutionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExcecutionService/ExcecutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExcecutionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcecutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
