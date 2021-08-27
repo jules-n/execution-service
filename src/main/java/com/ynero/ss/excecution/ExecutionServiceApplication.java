333cf0ed3cf15055b1a516ba4e3efff88d486a03
ynero <ynero@softserveinc.com>
2021-08-27 17:51:21 +0300

    feature/SS-54: Added response

--- src/main/java/com/ynero/ss/excecution/ExecutionServiceApplication.java
+++ src/main/java/com/ynero/ss/excecution/ExecutionServiceApplication.java
@@ -1,9 +1,5 @@
 package com.ynero.ss.excecution;
 
-import com.ynero.ss.excecution.services.receiver.gRPCReceiver;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
