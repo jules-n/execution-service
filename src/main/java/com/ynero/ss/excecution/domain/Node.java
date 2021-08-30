45c071797301fb85981dba9c0e029ff548a7b7b0
ynero <ynero@softserveinc.com>
2021-08-30 17:32:12 +0300

    feature/SS-30: WIP node domain model

---
+++ src/main/java/com/ynero/ss/excecution/domain/Node.java
@@ -0,0 +1,12 @@
+package com.ynero.ss.excecution.domain;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.UUID;
+
+@Document(collection = Node.COLLECTION_NAME)
+public class Node {
+    public static final String COLLECTION_NAME = "nodes";
+    @Indexed(unique = true)
+    private UUID nodeId;
+}
