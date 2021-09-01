4eb4c1a1d960d87e6937f9c4fa00f6a3ada5317c
ynero <ynero@softserveinc.com>
2021-09-01 15:20:18 +0300

    feature/SS-32: Added mapper

--- src/main/java/com/ynero/ss/excecution/config/ModelMapperConfig.java
+++ src/main/java/com/ynero/ss/excecution/config/ModelMapperConfig.java
@@ -1,14 +1,15 @@
-package com.ynero.ss.analytics_service.config;
+package com.ynero.ss.excecution.config;
 
-import com.ynero.ss.analytics_service.domain.Port;
-import com.ynero.ss.analytics_service.domain.PortsDataRecord;
+import com.ynero.ss.excecution.domain.Edge;
+import com.ynero.ss.excecution.domain.Pipeline;
+import com.ynero.ss.excecution.domain.dto.PipelineDTO;
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import dtos.PortValueDTO;
 
+import java.util.TreeSet;
 import java.util.UUID;
 
 @Configuration
@@ -17,24 +18,29 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.addConverter(portValueDTOToPortsDataRecordConverter);
+        modelMapper.addConverter(dtoPipelineConverter);
         return modelMapper;
     }
 
-    private Converter<PortValueDTO, PortsDataRecord> portValueDTOToPortsDataRecordConverter = new AbstractConverter<>() {
-        protected PortsDataRecord convert(PortValueDTO dto) {
-            var port = Port.builder()
-            .value(dto.getPortValue())
-            .name(dto.getPortName());
+    private Converter<PipelineDTO, Pipeline> dtoPipelineConverter = new AbstractConverter<>() {
+        protected Pipeline convert(PipelineDTO dto) {
+            var edges = new TreeSet<Edge>();
 
-            var record = PortsDataRecord.builder()
-            .deviceId(dto.getDeviceId())
-            .tenantId(dto.getTenantId())
-            .port(port.build())
-            .receivingTime(dto.getReceivingTime())
-            .recordId(UUID.randomUUID());
+            dto.getEdges().forEach(
+                    edge -> {
+                        edges.add(new Edge(
+                                UUID.fromString(edge.getNodeIdi()),
+                                UUID.fromString(edge.getNodeIdj())
+                        ));
+                    }
+            );
 
-            return record.build();
+            var pipeline = Pipeline.builder()
+                    .tenantId(dto.getTenantId())
+                    .edges(edges)
+                    .build();
+
+            return pipeline;
         }
     };
 }
