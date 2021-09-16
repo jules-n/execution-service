21d5c0c725c92f696c8ec189abd094cb342376e0
ynero <ynero@softserveinc.com>
2021-09-16 18:52:30 +0300

    feature/SS-80: Extended redis config

--- src/main/java/com/ynero/ss/execution/config/LettuceConfig.java
+++ src/main/java/com/ynero/ss/execution/config/LettuceConfig.java
@@ -1,18 +1,21 @@
 package com.ynero.ss.execution.config;
 
+import com.ynero.ss.execution.domain.Node;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import services.CacheService;
+import services.RedisWithPrefixOptionCacheServiceImpl;
 
 @Configuration
 public class LettuceConfig {
 
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String, Node> redisTemplate() {
+        RedisTemplate<String, Node> template = new RedisTemplate<>();
         template.setConnectionFactory(lettuceConnectionFactory());
         template.setHashKeySerializer(RedisSerializer.string());
         template.setHashValueSerializer(RedisSerializer.json());
@@ -24,5 +27,10 @@ public class LettuceConfig {
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
         return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public CacheService<String, Node> cacheService(){
+        return new RedisWithPrefixOptionCacheServiceImpl<String, Node>(redisTemplate(), "node: ");
     }
 }
