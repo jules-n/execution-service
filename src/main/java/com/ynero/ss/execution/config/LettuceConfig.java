817283b0a965ed39b1c5edf4be12705d26a97cdc
ynero <ynero@softserveinc.com>
2021-10-06 19:07:46 +0300

    feature/SS-33: Modified configuration of kafka and redis

--- src/main/java/com/ynero/ss/execution/config/LettuceConfig.java
+++ src/main/java/com/ynero/ss/execution/config/LettuceConfig.java
@@ -1,6 +1,8 @@
 package com.ynero.ss.execution.config;
 
 import com.ynero.ss.execution.domain.Node;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -12,6 +14,11 @@ import services.RedisWithPrefixOptionCacheServiceImpl;
 @Configuration
 public class LettuceConfig {
 
+    @Setter(onMethod_ = @Value("${spring.data.redis.host}"))
+    private String hostName;
+
+    @Setter(onMethod_ = @Value("${spring.data.redis.port}"))
+    private int port;
 
     @Bean
     public RedisTemplate<String, Node> redisTemplate() {
@@ -26,7 +33,12 @@ public class LettuceConfig {
 
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory();
+        var lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.getStandaloneConfiguration()
+                .setHostName(hostName);
+        lettuceConnectionFactory.getStandaloneConfiguration()
+                .setPort(port);
+        return lettuceConnectionFactory;
     }
 
     @Bean
