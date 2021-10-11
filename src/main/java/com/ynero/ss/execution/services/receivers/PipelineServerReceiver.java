f7d56208468573deba272ae7963fff362d3fe390
ynero <ynero@softserveinc.com>
2021-10-11 12:14:18 +0300

    feature/SS-33: Added shutdown hook

--- src/main/java/com/ynero/ss/execution/services/receivers/PipelineServerReceiver.java
+++ src/main/java/com/ynero/ss/execution/services/receivers/PipelineServerReceiver.java
@@ -7,6 +7,7 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import lombok.Setter;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ import javax.annotation.PreDestroy;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@Log4j2
 public class PipelineServerReceiver {
 
     @Setter(onMethod_ = {@Value("${grpc.server.port}")})
@@ -31,6 +33,10 @@ public class PipelineServerReceiver {
                 .addService(new NodeCRUDReceiver())
                 .build();
         server.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            server.shutdown();
+            log.info("Successfully stopped the server");
+        }));
     }
 
     @SneakyThrows
