62be4b53480c4b6269bd55e903eabc1121286a02
ynero <ynero@softserveinc.com>
2021-10-12 16:38:42 +0300

    feature/SS-33: Changed lettuce to jedis

--- src/main/java/com/ynero/ss/execution/config/JedisConfig.java
+++ src/main/java/com/ynero/ss/execution/config/JedisConfig.java
@@ -1,35 +1,30 @@
 package com.ynero.ss.execution.config;
 
 import com.ynero.ss.execution.domain.Node;
-import io.lettuce.core.RedisURI;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import services.CacheService;
 import services.RedisWithPrefixOptionCacheServiceImpl;
 
-import java.util.concurrent.TimeUnit;
-
 @Configuration
-public class LettuceConfig {
+public class JedisConfig {
 
-    @Setter(onMethod_ = @Value("${spring.data.redis.host}"))
+    @Setter(onMethod_ = @Value("${spring.redis.host}"))
     private String hostName;
 
-    @Setter(onMethod_ = @Value("${spring.data.redis.port}"))
+    @Setter(onMethod_ = @Value("${spring.redis.port}"))
     private int port;
 
     @Bean
     public RedisTemplate<String, Node> redisTemplate() {
         RedisTemplate<String, Node> template = new RedisTemplate<>();
-        template.setConnectionFactory(lettuceConnectionFactory());
+        template.setConnectionFactory(jedisConnectionFactory());
         template.setHashKeySerializer(RedisSerializer.string());
         template.setHashValueSerializer(RedisSerializer.json());
         template.setKeySerializer(RedisSerializer.string());
@@ -38,14 +33,9 @@ public class LettuceConfig {
     }
 
     @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory() {
-        var lettuceConnectionFactory = new LettuceConnectionFactory();
-        lettuceConnectionFactory.getStandaloneConfiguration()
-                .setHostName(hostName);
-        lettuceConnectionFactory.getStandaloneConfiguration()
-                .setPort(port);
-        lettuceConnectionFactory.setTimeout(6000);
-        return lettuceConnectionFactory;
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(hostName, port);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
