a007c7efff8303a2bbe931c02ca4d643ae3125c1
ynero <ynero@softserveinc.com>
2021-11-30 12:54:49 +0200

    Basic config for security

---
+++ src/main/java/com/ynero/ss/execution/security/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.ynero.ss.execution.security;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableConfigurationProperties
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Setter(onMethod_ = {@Autowired})
+    com.ynero.ss.execution.security.UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers(HttpMethod.POST,"/users")
+                .permitAll();
+        http.csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic()
+                .and().sessionManagement().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder builder)
+            throws Exception {
+        builder.userDetailsService(userDetailsService);
+    }
+}
---
+++ src/main/java/com/ynero/ss/execution/security/UserDetailsService.java
@@ -0,0 +1,33 @@
+package com.ynero.ss.execution.security;
+import com.ynero.ss.execution.domain.User;
+import com.ynero.ss.execution.persistence.user.UserRepository;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Setter(onMethod_ = {@Autowired})
+    private UserRepository repository;
+
+    @Override
+    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = repository.findByUsername(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("No such user");
+        }
+
+        List<SimpleGrantedAuthority> authorities = user.getRoles().stream().map(
+                role -> new SimpleGrantedAuthority(role)
+        ).collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
+}
